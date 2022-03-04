4e091972f6b3df6375554128b8e7089853df9bc3
Kevin Wainwright <mr_rich199@hotmail.com>
2022-03-04 11:32:24 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string and press enter: ");
+            string readInput = Console.ReadLine();
+            Console.WriteLine("You have entered {0}",readInput);
+
+            Console.Write("Enter a Key and press Enter:");
+            int asciiValue = Console.Read();
+            Console.WriteLine("ASCII value is {0}",asciiValue);
+            Console.ReadKey();
+        }
+    }
+}
